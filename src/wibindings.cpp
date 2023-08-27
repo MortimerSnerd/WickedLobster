1d8d4459b916e93a2938fae74f5eed0da1bb7d7d
Mortimer Snerd <Pat.Kelley@gmail.com>
2023-08-27 10:44:48 -0400

    Change key for scenes from array index to pointer
    Both user created scenes and the global scene have stable addresses,
    so use the address for the key and throw out the indirection to the
    array.
    
    Also, added support for creating a new entity.

--- src/wibindings.cpp
+++ src/wibindings.cpp
@@ -13,20 +13,8 @@
 using namespace std;
 
 
-namespace
-{
-    const int64_t GLOBAL_SCENE_IX = -1;
-    // Array of scenes created by Lobster code.  Indexed by wo_handle.name.
-    vector<wi::scene::Scene*> g_scenes;
-}
-
 namespace wbnd
 {
-    template<typename T>
-    bool handle_array_check(vector<T> arr, int64_t ix)
-    {
-        return ix >= 0 && ix < (int64_t)arr.size();
-    }
     void handle_check(wo_handle h, WOKIND kind)
     {
         if (h.kind != kind) {
@@ -36,47 +24,49 @@ namespace wbnd
             fflush(stdout);
             abort();
         }
-        switch (kind) {
+        switch (h.kind) {
         case WK_SCENE:
-            if (h.name != GLOBAL_SCENE_IX && !handle_array_check(g_scenes, h.name)) {
-                printf("Bad scene handle: %d\n", (int)h.name);
-                dump_lobster_stack(); 
+            if (h.name == 0) {
+                printf("Null scene pointer.");
+                dump_lobster_stack();
                 fflush(stdout);
                 abort();
             }
         }
     }
 
-    wi::scene::Scene& scene_for_ix(int64_t i)
+    wi::scene::Scene* scene_for_ix(int64_t i)
     {
-        if (i == GLOBAL_SCENE_IX) {
-            return wi::scene::GetScene();
-        } else {
-            return *g_scenes[i];
-        }
+        return reinterpret_cast<wi::scene::Scene *>(i);
     }
 
     wo_handle new_scene()
     {
-        auto ix = g_scenes.size();
-        g_scenes.push_back(new wi::scene::Scene());
-        return wo_handle{WK_SCENE, (int64_t)ix};
+        // We create scenes, or return pointers to a global scene, neither
+        // which will move.  So it's safe to use the address as the key.
+        auto sc = new wi::scene::Scene();
+        return {WK_SCENE, reinterpret_cast<int64_t>(sc)};
     }
 
     wo_handle global_scene()
     {
-        return wo_handle{WK_SCENE, GLOBAL_SCENE_IX};
+        return wo_handle{WK_SCENE, reinterpret_cast<int64_t>(&wi::scene::GetScene())};
     }
 
     wo_handle load_model(wo_handle dest_scene, std::string const& filename, bool attach_to_entity)
     {
         handle_check(dest_scene, WK_SCENE);
-        auto ent = wi::scene::LoadModel(scene_for_ix(dest_scene.name), filename,
+        auto ent = wi::scene::LoadModel(*scene_for_ix(dest_scene.name), filename,
                                         XMMatrixIdentity(), attach_to_entity);
         if (ent == wi::ecs::INVALID_ENTITY) {
             return {-WK_ENTITY, 0};
         }
         return {WK_ENTITY, (int64_t)ent};
+    }
+
+    wo_handle create_entity()
+    {
+        return {WK_ENTITY, wi::ecs::CreateEntity()};
     }
 }
 
--- src/wibindings.h
+++ src/wibindings.h
@@ -7,5 +7,6 @@ namespace wbnd
     wo_handle new_scene();
     wo_handle global_scene();
     wo_handle load_model(wo_handle dest_scene, std::string const &filename, bool attach_to_entity);
+    wo_handle create_entity();
 }
 
